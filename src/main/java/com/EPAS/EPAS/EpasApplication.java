f5c9933b48b293ee4f84b8b2ce9f20cb470df059
sayali8397 <sayali8397@gmail.com>
2018-07-25 16:50:50 +0530

    Creating project

---
+++ src/main/java/com/EPAS/EPAS/EpasApplication.java
@@ -0,0 +1,12 @@
+package com.EPAS.EPAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/EPAS/EPAS/EpasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.EPAS.EPAS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
